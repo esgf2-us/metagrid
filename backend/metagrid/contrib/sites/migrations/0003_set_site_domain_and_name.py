7e9268d046b3a7021dd08f31506ed985ae951077
Tom Vo <tomvothecoder@gmail.com>
2020-03-05 09:59:23 -0800

    Update .pre-commit-config.yaml

--- backend/metagrid/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ backend/metagrid/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -11,11 +11,7 @@ def update_site_forward(apps, schema_editor):
     """Set site domain and name."""
     Site = apps.get_model("sites", "Site")
     Site.objects.update_or_create(
-        id=settings.SITE_ID,
-        defaults={
-            "domain": "example.com",
-            "name": "MetaGrid",
-        },
+        id=settings.SITE_ID, defaults={"domain": "example.com", "name": "MetaGrid",},
     )
 
 
