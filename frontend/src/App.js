041dd0aabe1b830939e3d35a41db97eaa864e8c6
tomvothecoder <tomvothecoder@gmail.com>
2020-03-23 16:59:13 -0700

    Add ant design layout for the sider and content body

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-
+import { Layout } from "antd";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Search from "./components/search/Search";
+
+const { Header, Content, Sider } = Layout;
 
 export default class App extends Component {
   state = {
@@ -13,6 +15,7 @@ export default class App extends Component {
   handleProjectChange = value => {
     this.setState({ project: value });
   };
+
   handleSubmitText = text => {
     this.setState({
       textInputs: [...this.state.textInputs, text]
@@ -27,6 +30,11 @@ export default class App extends Component {
     });
   };
 
+  handleClearTags = () => {
+    // TODO: Implement method
+    this.setState({ project: "", textInputs: [] });
+  };
+
   render() {
     return (
       <div>
@@ -34,12 +42,26 @@ export default class App extends Component {
           onProjectChange={this.handleProjectChange}
           onSearch={text => this.handleSubmitText(text)}
         ></NavBar>
-        <Search
-          project={this.state.project}
-          textInputs={this.state.textInputs}
-          onRemoveTag={removedTag => this.handleRemoveTag(removedTag)}
-        ></Search>
+        <Layout style={{ padding: "24px 0" }}>
+          <Content>
+            <Layout>
+              <Sider style={layoutDefaults} width={200}></Sider>
+              <Content style={{ padding: "0 24px", minHeight: 280 }}>
+                <Search
+                  project={this.state.project}
+                  textInputs={this.state.textInputs}
+                  onRemoveTag={removedTag => this.handleRemoveTag(removedTag)}
+                  onClearTags={() => this.handleClearTags()}
+                ></Search>
+              </Content>
+            </Layout>
+          </Content>
+        </Layout>
       </div>
     );
   }
 }
+
+const layoutDefaults = {
+  background: "#fff"
+};
