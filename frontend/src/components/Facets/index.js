311cae49f8a64e2d3dab2676b387b1e1a238831e
tomvothecoder <tomvothecoder@gmail.com>
2020-03-25 16:46:23 -0700

    Add Facets component - #3 - Add temporary placeholders for facet filtering using Select and CheckBoxGroup components - Add facets.json mock data to render example facet

---
+++ frontend/src/components/Facets/index.js
@@ -0,0 +1,84 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Form, Button, Checkbox, Select, Row, Col, Spin } from "antd";
+import { FilterOutlined } from "@ant-design/icons";
+
+import facetsJson from "../../facets.json";
+
+const { Option } = Select;
+const CheckboxGroup = Checkbox.Group;
+
+function fetchFacets() {
+  // TODO: Call an API instead of mock data
+  return JSON.parse(JSON.stringify(facetsJson));
+}
+
+function Facets({ onAddFacet }) {
+  Facets.propTypes = {
+    onAddFacet: PropTypes.func.isRequired
+  };
+
+  const [facets, setFacets] = React.useState([]);
+
+  // TODO: Combine with facets
+  const [checkedList, setCheckedList] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [form] = Form.useForm();
+
+  React.useEffect(() => {
+    setFacets(() => fetchFacets());
+    setLoading(false);
+  }, []);
+
+  const handleChange = value => {
+    // TODO: Implement this method
+    console.log(`selected ${value}`);
+  };
+
+  const onChange = checkedList => {
+    setCheckedList(checkedList);
+  };
+
+  if (loading) {
+    return <Spin></Spin>;
+  }
+
+  return (
+    <div>
+      <Row>
+        <Col>
+          <Form form={form} layout="vertical">
+            <Form.Item name="sourceid" label="Source ID">
+              <Select
+                mode="tags"
+                style={{ width: "100%" }}
+                onChange={handleChange}
+                tokenSeparators={[","]}
+                showArrow={true}
+              >
+                {facets.source_ids.map((child, index) => {
+                  return <Option key={index}>{child}</Option>;
+                })}
+              </Select>
+            </Form.Item>
+
+            <Form.Item name="sourceid" label="Source ID">
+              <CheckboxGroup
+                options={facets.so
+                  To ignore, add // eslint-disable-next-line to the line before.
+                  To ignore, add // eslint-disable-next-line to the line before.urce_ids}
+                value={checkedList}
+                onChange={onChange}
+              />
+            </Form.Item>
+            <Button type="primary" htmlType="submit">
+              <FilterOutlined /> Apply Facets
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default Facets;
