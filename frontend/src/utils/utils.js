23ff16ac31b6370d65268b4cd6e9f9447c652b71
tomvothecoder <tomvothecoder@gmail.com>
2020-03-26 16:14:13 -0700

    Add utils directory for shared utils - Add humanize function for updating JSON facet labels - TODO: Write a smarter function since it ignores casing

---
+++ frontend/src/utils/utils.js
@@ -0,0 +1,8 @@
+export function humanize(str) {
+  let i,
+    frags = str.split("_");
+  for (i = 0; i < frags.length; i++) {
+    frags[i] = frags[i].charAt(0).toUpperCase() + frags[i].slice(1);
+  }
+  return frags.join(" ");
+}
