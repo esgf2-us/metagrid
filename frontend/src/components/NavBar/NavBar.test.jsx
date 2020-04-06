1974399256d8ee25b68afee0de308ec2aff1908f
tomvothecoder <tomvothecoder@gmail.com>
2020-04-06 14:48:07 -0700

    Add notes to NavBar tests

--- frontend/src/components/NavBar/NavBar.test.jsx
+++ frontend/src/components/NavBar/NavBar.test.jsx
@@ -29,19 +29,29 @@ const leftMenuProps = {
 };
 
 test('LeftMenu renders project list and updates', async () => {
+  // NOTE:testing ant-design's select component has been proven to be
+  // tricky. Attempting to extract the value that the user selects is not
+  // straight-forward, so this project does a simple test to see if the
+  // DOM node exists or not.
   const { getByText, getByTestId } = render(<LeftMenu {...leftMenuProps} />);
   expect(getByTestId('left-menu')).toBeTruthy();
   expect(getByText('Project')).toBeTruthy();
   fireEvent.click(getByTestId('project-select'));
-  // TODO: project-options render outside of the component, so use entire container and select
 });
 
 test('LeftMenu registers search input', async () => {
-  const { getByPlaceholderText } = render(<LeftMenu {...leftMenuProps} />);
+  // NOTE: Since the Select component can't be set, this test only checks if
+  // the Search form field's value changes. It does not test calling the
+  // onFinish function when the user submits the form.
+  const { getByPlaceholderText, getByRole } = render(
+    <LeftMenu {...leftMenuProps} />
+  );
 
   const search = getByPlaceholderText('Search...');
   fireEvent.change(search, { target: { value: 'Solar' } });
   expect(search.value).toBe('Solar');
+
+  fireEvent.click(getByRole('img'));
 });
 
 test('RightMenu displays correct number of cartItems', () => {
@@ -50,5 +60,6 @@ test('RightMenu displays correct number of cartItems', () => {
 });
 
 test('RightMenu links redirect', () => {
+  // TODO: After integrating react-router and respective links
   test.todo('placeholder');
 });
