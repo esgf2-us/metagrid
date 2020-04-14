5b3c2bdd3301a2b7b848195cc2d8166cdb1c4cc9
tomvothecoder <tomvothecoder@gmail.com>
2020-04-14 13:52:40 -0700

    Update handleClearTags function - Display number of results found - Add alert based on if project/constraints are appled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,11 +41,9 @@ function App() {
   };
 
   /**
-   * Handles clearing all of the constraints applied by the user.
+   * Handles clearing constraints for a selected project.
    */
   const handleClearTags = () => {
-    // TODO: Implement method
-    setProject('');
     setTextInputs([]);
     setAppliedFacets({});
   };
