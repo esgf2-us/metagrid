ec7d6faed1cf8faaf4aa25100507168e3859684d
tomvothecoder <tomvothecoder@gmail.com>
2020-04-21 15:51:31 -0700

    Update config files to not use django-configurations - Add comments and links to config settings - Remove django-configurations and dj-database-url packages - Add django-environ package

--- backend-api/metagrid/config/production.py
+++ backend-api/metagrid/config/production.py
@@ -1,33 +1,19 @@
-import os
+from .base import *  # noqa
+from .base import env
 
-from .common import Common
+# GENERAL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
 
+# DATABASES
+# ------------------------------------------------------------------------------
+DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
+DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
+DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 
-class Production(Common):
-    INSTALLED_APPS = Common.INSTALLED_APPS
-    SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
-    # Site
-    # https://docs.djangoproject.com/en/2.0/ref/settings/#allowed-hosts
-    ALLOWED_HOSTS = ["*"]
-    INSTALLED_APPS += ("gunicorn",)
-
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/2.0/howto/static-files/
-    # http://django-storages.readthedocs.org/en/latest/index.html
-    INSTALLED_APPS += ("storages",)
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-    AWS_ACCESS_KEY_ID = os.getenv("DJANGO_AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("DJANGO_AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = os.getenv("DJANGO_AWS_STORAGE_BUCKET_NAME")
-    AWS_DEFAULT_ACL = "public-read"
-    AWS_AUTO_CREATE_BUCKET = True
-    AWS_QUERYSTRING_AUTH = False
-    MEDIA_URL = f"https://s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/"
-
-    # https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/http-caching#cache-control
-    # Response can be cached by browser and any intermediary caches (i.e. it is "public") for up to 1 day
-    # 86400 = (60 seconds x 60 minutes x 24 hours)
-    AWS_HEADERS = {
-        "Cache-Control": "max-age=86400, s-maxage=86400, must-revalidate",
-    }
+# APPS
+# ------------------------------------------------------------------------------
+INSTALLED_APPS += ["gunicorn"]  # noqa F405
