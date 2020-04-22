e9a2e51395407cc9277d8440c618130b5f8a5bb9
tomvothecoder <tomvothecoder@gmail.com>
2020-04-22 13:23:53 -0700

    Update test suite to use pytest - Update setup.cfg to use metagrid.config.test

--- backend/metagrid/users/test/test_serializers.py
+++ backend/metagrid/users/test/test_serializers.py
@@ -1,27 +1,29 @@
+import pytest
 from django.contrib.auth.hashers import check_password
 from django.forms.models import model_to_dict
-from django.test import TestCase
-from nose.tools import eq_, ok_
 
 from ..serializers import CreateUserSerializer
 from .factories import UserFactory
 
+pytestmark = pytest.mark.django_db
 
-class TestCreateUserSerializer(TestCase):
+
+class TestCreateUserSerializer:
+    @pytest.fixture(autouse=True)
     def setUp(self):
         self.user_data = model_to_dict(UserFactory.build())
 
     def test_serializer_with_empty_data(self):
         serializer = CreateUserSerializer(data={})
-        eq_(serializer.is_valid(), False)
+        assert not serializer.is_valid()
 
     def test_serializer_with_valid_data(self):
         serializer = CreateUserSerializer(data=self.user_data)
-        ok_(serializer.is_valid())
+        assert serializer.is_valid()
 
     def test_serializer_hashes_password(self):
         serializer = CreateUserSerializer(data=self.user_data)
-        ok_(serializer.is_valid())
+        assert serializer.is_valid()
 
         user = serializer.save()
-        ok_(check_password(self.user_data.get("password"), user.password))
+        assert check_password(self.user_data.get("password"), user.password)
