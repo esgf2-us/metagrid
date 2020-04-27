cfabe86ce64843fecc906222d74ad84da112b5cc
tomvothecoder <tomvothecoder@gmail.com>
2020-04-27 13:22:46 -0700

    Add axios default baseURL - References localhost:8000, which is the default url for Django servers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
 import App from './App';
+
+axios.defaults.baseURL = 'http://localhost:8000';
 
 ReactDOM.render(<App />, document.getElementById('root'));
