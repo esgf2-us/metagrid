ff8df65f9d10e758818eb28de149f4093a94785e
tomvothecoder <tomvothecoder@gmail.com>
2020-04-27 13:23:58 -0700

    Fix Projects app urls - Register Project model with admin

--- backend/metagrid/projects/urls.py
+++ backend/metagrid/projects/urls.py
@@ -4,6 +4,6 @@ from .views import ProjectFacetsList, ProjectsList
 
 app_name = "projects"
 urlpatterns = [
-    path("/", view=ProjectsList.as_view(), name="list"),
-    path("/facets", view=ProjectFacetsList.as_view(), name="list_facets"),
+    path("", view=ProjectsList.as_view(), name="list"),
+    path("facets", view=ProjectFacetsList.as_view(), name="list_facets"),
 ]
