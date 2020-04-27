bc918be8ab0a2556c84b92b38eb394c0888cc3d3
tomvothecoder <tomvothecoder@gmail.com>
2020-04-27 13:23:58 -0700

    Fix Projects app urls - Register Project model with admin

--- backend/metagrid/projects/admin.py
+++ backend/metagrid/projects/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
 
-# Register your models here.
+from .models import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
