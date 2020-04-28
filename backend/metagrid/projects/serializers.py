6cd25dd56d84b698b2c5807b85d4f7dd4b5b6b99
tomvothecoder <tomvothecoder@gmail.com>
2020-04-28 10:05:34 -0700

    Add StringRelatedField "facets" to ProjectSerializer - Add prefetch_related() to ProjectsViewSet queryset

--- backend/metagrid/projects/serializers.py
+++ backend/metagrid/projects/serializers.py
@@ -4,6 +4,8 @@ from .models import Project
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    facets = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = Project
         fields = ("name", "facets")
--- backend/metagrid/projects/views.py
+++ backend/metagrid/projects/views.py
@@ -9,7 +9,7 @@ from .serializers import ProjectSerializer
 
 
 class ProjectsViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Project.objects.all()
+    queryset = Project.objects.all().prefetch_related()
     serializer_class = ProjectSerializer
     permission_classes = [AllowAny]
     lookup_field = "name"
