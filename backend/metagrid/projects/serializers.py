b40dc29ec5632105143d7c3d1392dd1b6b851cee
tomvothecoder <tomvothecoder@gmail.com>
2020-04-29 15:40:41 -0700

    Add facets_url property to ProjectSerializer

--- backend/metagrid/projects/serializers.py
+++ backend/metagrid/projects/serializers.py
@@ -5,7 +5,8 @@ from .models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
     facets = serializers.StringRelatedField(many=True)
+    facets_url = serializers.ReadOnlyField
 
     class Meta:
         model = Project
-        fields = ("name", "facets")
+        fields = ("name", "full_name", "description", "facets", "facets_url")
