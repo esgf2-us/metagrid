273eda03915aee8fb10b5cd6e6bed7f9dd8e63ff
tomvothecoder <tomvothecoder@gmail.com>
2020-04-29 16:24:09 -0700

    Fix projects tests - Caused by the facets_url property - Add a RelatedFactory to the ProjectFactory to resolve

--- backend/metagrid/projects/tests/factories.py
+++ backend/metagrid/projects/tests/factories.py
@@ -1,17 +1,18 @@
 import factory
 
 
-class ProjectFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = "projects.Project"
-        django_get_or_create = ("name",)
-
-    name = factory.Faker("company")
-
-
 class FacetFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = "projects.Facet"
         django_get_or_create = ("name",)
 
     name = factory.Faker("job")
+
+
+class ProjectFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "projects.Project"
+        django_get_or_create = ("name",)
+
+    name = factory.Faker("company")
+    facets = factory.RelatedFactory(FacetFactory, "project")
