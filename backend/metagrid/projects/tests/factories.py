2e9a4f3152f7f84d90db3b270ba107230dcdf1ff
tomvothecoder <tomvothecoder@gmail.com>
2020-04-28 17:06:48 -0700

    Add tests for projects views and urls - Update project test factories to include Faker

--- backend/metagrid/projects/tests/factories.py
+++ backend/metagrid/projects/tests/factories.py
@@ -6,8 +6,12 @@ class ProjectFactory(factory.django.DjangoModelFactory):
         model = "projects.Project"
         django_get_or_create = ("name",)
 
+    name = factory.Faker("company")
+
 
 class FacetFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = "projects.Facet"
         django_get_or_create = ("name",)
+
+    name = factory.Faker("job")
