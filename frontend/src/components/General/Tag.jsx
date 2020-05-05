3bc521db37ab2526193b6a8391a86241564ed867
tomvothecoder <tomvothecoder@gmail.com>
2020-05-05 11:00:28 -0700

    Update Facets component to reset form fields - Based on the appliedFacets prop - Update handleRemoveTag to filter appliedFacets based on the removedTag - Update Tag object to accept children props

--- frontend/src/components/General/Tag.jsx
+++ frontend/src/components/General/Tag.jsx
@@ -6,32 +6,34 @@ const styles = {
   tag: { height: '2em' },
 };
 
-function Tag({ input, onClose, closable, type }) {
-  Tag.propTypes = {
-    input: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.arrayOf(
-        PropTypes.oneOfType([PropTypes.number, PropTypes.string])
-      ),
-    ]).isRequired,
-    onClose: PropTypes.func.isRequired,
-    closable: PropTypes.bool,
-    type: PropTypes.string.isRequired,
-  };
-
-  Tag.defaultProps = {
-    closable: true,
-  };
-
+function Tag({ value, onClose, closable, type, children }) {
   return (
     <TagD
       style={styles.tag}
       closable={closable}
-      onClose={() => onClose(input, type)}
+      onClose={() => onClose(value, type)}
     >
-      {input.constructor === Array ? input[0] : input}
+      {children}
     </TagD>
   );
 }
+
+Tag.propTypes = {
+  value: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.objectOf(PropTypes.string),
+    PropTypes.arrayOf(
+      PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+    ),
+  ]).isRequired,
+  onClose: PropTypes.func.isRequired,
+  closable: PropTypes.bool,
+  type: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+Tag.defaultProps = {
+  closable: true,
+};
 
 export default Tag;
