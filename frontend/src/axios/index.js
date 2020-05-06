667feebbe5b6d2af0d066d3115bb7efe598aa637
tomvothecoder <tomvothecoder@gmail.com>
2020-05-06 11:17:18 -0700

    Add axios adapter - Source: https://stackoverflow.com/questions/51054286/cross-origin-http-request-originating-from-server-side-nodejs-axios-jsdom

--- frontend/src/axios/index.js
+++ frontend/src/axios/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
+import httpAdapter from 'axios/lib/adapters/http';
 
 // TODO:: use environment variables, based on development environments
 export default axios.create({
+  // eslint-disable-next-line global-require
+  adapter: httpAdapter,
   baseURL: 'http://localhost:8000',
 });
