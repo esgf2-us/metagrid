1fd639a84ea1f4a8d7e52e7e89fb305af13be86e
tomvothecoder <tomvothecoder@gmail.com>
2020-05-15 15:51:52 -0700

    Add styling and images to the Summary component - Add svgs from flaticon: https://www.flaticon.com/packs/essential-collection/

--- frontend/src/components/Cart/Summary.jsx
+++ frontend/src/components/Cart/Summary.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Divider, Form, Select } from 'antd';
-import { DownloadOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import { DownloadOutlined } from '@ant-design/icons';
 
 import Button from '../General/Button';
 
-const { Option } = Select;
+import cartImg from '../../assets/img/cart.svg';
+import dataImg from '../../assets/img/data.svg';
+import folderImg from '../../assets/img/folder.svg';
+
+const styles = {
+  headerContainer: { display: 'flex', justifyContent: 'center' },
+  summaryHeader: { fontWeight: 'bold', textAlign: 'center' },
+  image: { margin: '1em', width: '25%' },
+  statistic: { float: 'right' },
+};
 
 function Summary({ numItems, numFiles }) {
   const [form] = Form.useForm();
@@ -22,42 +31,56 @@ function Summary({ numItems, numFiles }) {
 
   return (
     <div data-testid="summary">
-      <ShoppingCartOutlined style={{ fontSize: '4rem' }} />
-      <h1>Data Cart Summary</h1>
+      <div style={styles.headerContainer}>
+        <img style={styles.image} src={cartImg} alt="Cart" />
+        <img style={styles.image} src={folderImg} alt="Folder" />
+      </div>
+
+      <h1 style={styles.summaryHeader}>Your Cart Summary</h1>
+
       <Divider />
       <h1>
-        Number of Datasets: <span style={{ float: 'right' }}>{numItems}</span>
+        Number of Datasets: <span style={styles.statistic}>{numItems}</span>
       </h1>
       <h1>
-        Number of Files: <span style={{ float: 'right' }}>{numFiles}</span>
+        Number of Files: <span style={styles.statistic}>{numFiles}</span>
       </h1>
       <h1>
-        Total File Size: <span style={{ float: 'right' }}>N/A</span>
+        Total File Size: <span style={styles.statistic}>N/A</span>
       </h1>
       <Divider />
+      <div style={styles.headerContainer}>
+        <img style={styles.image} src={dataImg} alt="Data" />
+      </div>
+      <h1 style={styles.summaryHeader}>Download Your Cart</h1>
+      <p>
+        Download speeds will vary based on your bandwidth and distance from the
+        data node serving the file(s)
+      </p>
       <Form
         form={form}
-        layout="vertical"
+        layout="inline"
         onFinish={(values) => handleOnFinish(values)}
         initialValues={{
           download: downloadOptions[0],
         }}
       >
         <Form.Item name="download">
-          <Select style={{ width: 120 }}>
-            {/* eslint-disable-next-line react/prop-types */}
+          <Select style={{ width: 235 }}>
             {downloadOptions.map((option) => (
-              <Option key={option} value={option}>
+              <Select.Option key={option} value={option}>
                 {option}
-              </Option>
+              </Select.Option>
             ))}
             /
           </Select>
         </Form.Item>
         <Form.Item>
-          <Button type="primary" htmlType="submit" icon={<DownloadOutlined />}>
-            Download
-          </Button>
+          <Button
+            type="primary"
+            htmlType="submit"
+            icon={<DownloadOutlined />}
+          ></Button>
         </Form.Item>
       </Form>
     </div>
