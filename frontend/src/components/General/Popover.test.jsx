e86544674531f6e68e1bdf5086ca63d3e4aa3b45
tomvothecoder <tomvothecoder@gmail.com>
2020-05-19 11:22:33 -0700

    Update Popover test

--- frontend/src/components/General/Popover.test.jsx
+++ frontend/src/components/General/Popover.test.jsx
@@ -4,13 +4,18 @@ import { fireEvent, render } from '@testing-library/react';
 import Popover from './Popover';
 
 it('returns component with required content', () => {
-  const { getByText, findByText } = render(
+  const { getByText, getByRole, findByText } = render(
     <Popover content={<p>foobar</p>} trigger="click">
       Click Me
     </Popover>
   );
+
+  // Trigger event
   const popOverBtn = getByText('Click Me');
   fireEvent.click(popOverBtn);
 
+  // Check if popOver exists
+  const popOver = getByRole('tooltip');
+  expect(popOver).toBeTruthy();
   expect(findByText('foobar')).toBeTruthy();
 });
