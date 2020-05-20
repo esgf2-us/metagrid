29093c0c4c132cc890ef64dc4b403f8467652a80
tomvothecoder <tomvothecoder@gmail.com>
2020-05-20 15:06:47 -0700

    Add 100% coverage for Cart components

--- frontend/src/components/Cart/Summary.test.jsx
+++ frontend/src/components/Cart/Summary.test.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Summary from './Summary';
 
 const defaultProps = {
-  numItems: 5,
+  numItems: 2,
+  numFiles: 5,
 };
 
 test('renders without crashing', () => {
@@ -16,4 +17,26 @@ test('renders without crashing', () => {
     </Router>
   );
   expect(getByTestId('summary')).toBeTruthy();
+});
+
+it('shows the correct number of datasets and files', () => {
+  const { getByText } = render(
+    <Router>
+      <Summary {...defaultProps} />
+    </Router>
+  );
+  // Shows number of files
+  expect(getByText('2')).toBeTruthy();
+  expect(getByText('5')).toBeTruthy();
+});
+
+it('handles the download form submission with handleOnFinish()', () => {
+  const { getByRole } = render(
+    <Router>
+      <Summary {...defaultProps} />
+    </Router>
+  );
+  // Shows number of files
+  const downloadBtn = getByRole('img', { name: 'download' });
+  fireEvent.submit(downloadBtn);
 });
