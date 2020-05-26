90241b56ca16fca8196d1e25a130cd72b3c125f0
tomvothecoder <tomvothecoder@gmail.com>
2020-05-26 16:55:21 -0700

    Add overrides to settings to target TypeScript - This fixes issues related to TypeScript checks being performed on JavaScript files - https://github.com/typescript-eslint/typescript-eslint/issues/964#issuecomment-569481854

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -10,18 +10,12 @@ module.exports = {
     'airbnb/hooks',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
     'prettier/@typescript-eslint',
     'prettier/react',
   ],
-  overrides: [
-    {
-      files: ['**/*.tsx'],
-      rules: {
-        'react/prop-types': 'off',
-      },
-    },
-  ],
+
   plugins: ['react', 'jsx-a11y', 'import'],
   parserOptions: {
     project: './tsconfig.json',
@@ -34,14 +28,34 @@ module.exports = {
       { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
     ],
     'react/self-closing-comp': 'off',
-    // Makes no sense to allow type inferrence for expression parameters, but require typing the response (https://github.com/iamturns/create-exposed-app/blob/master/.eslintrc.js)
-    '@typescript-eslint/explicit-function-return-type': [
-      'error',
-      { allowExpressions: true, allowTypedFunctionExpressions: true },
-    ],
-    '@typescript-eslint/no-use-before-define': [
-      'error',
-      { functions: false, classes: true, variables: true, typedefs: true },
-    ],
+    // Disable these rules for all files since the project is mixed with JavaScript and TypeScript.
+    // Overrides are used to target TypeScript files only.
+    // TODO:: Remove these configurations once TypeScript conversion is fully completed
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/unbound-method': 'off',
   },
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+    // https://github.com/typescript-eslint/typescript-eslint/issues/964
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': ['error'],
+        // Makes no sense to allow type inferrence for expression parameters, but require typing the response (https://github.com/iamturns/create-exposed-app/blob/master/.eslintrc.js)
+        '@typescript-eslint/explicit-function-return-type': [
+          'error',
+          { allowExpressions: true, allowTypedFunctionExpressions: true },
+        ],
+        '@typescript-eslint/no-use-before-define': [
+          'error',
+          { functions: false, classes: true, variables: true, typedefs: true },
+        ],
+      },
+    },
+  ],
 };
