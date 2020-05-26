a711bcee51cb2e1e0196e8d0432be3a1212713fd
tomvothecoder <tomvothecoder@gmail.com>
2020-05-26 15:38:04 -0700

    Add packages and update .eslintrc to allow ESLint support TypeScript

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,21 +1,48 @@
 module.exports = {
-  extends: [
-    'airbnb',
-    'airbnb/hooks',
-    'plugin:prettier/recommended',
-    'prettier/react',
-  ],
-  plugins: ['react', 'jsx-a11y', 'import'],
-  rules: {
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    'react/self-closing-comp': 'off',
-    'prettier/prettier': 'error',
-  },
   env: {
     browser: true,
     es6: true,
     jest: true,
     node: true,
+  },
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  overrides: [
+    {
+      files: ['**/*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+  plugins: ['react', 'jsx-a11y', 'import'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  rules: {
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'react/destructuring-assignment': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'react/self-closing-comp': 'off',
+    'prettier/prettier': 'error',
+    // Makes no sense to allow type inferrence for expression parameters, but require typing the response (https://github.com/iamturns/create-exposed-app/blob/master/.eslintrc.js)
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      { allowExpressions: true, allowTypedFunctionExpressions: true },
+    ],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      { functions: false, classes: true, variables: true, typedefs: true },
+    ],
   },
 };
