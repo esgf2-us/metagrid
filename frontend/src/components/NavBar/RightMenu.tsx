c87c156423ac2c9c95d965922a31d3a2aee8a717
tomvothecoder <tomvothecoder@gmail.com>
2020-05-27 13:55:07 -0700

    Convert RightMenu component to .tsx

--- frontend/src/components/NavBar/RightMenu.tsx
+++ frontend/src/components/NavBar/RightMenu.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import { Menu } from 'antd';
 import { ShoppingCartOutlined } from '@ant-design/icons';
 
 import Button from '../General/Button';
 
-const { SubMenu } = Menu;
+type Props = {
+  mode:
+    | 'horizontal'
+    | 'vertical'
+    | 'vertical-left'
+    | 'vertical-right'
+    | 'inline';
+  cartItems: number;
+};
 
-function RightMenu({ mode, cartItems }) {
-  RightMenu.propTypes = {
-    mode: PropTypes.string.isRequired,
-    cartItems: PropTypes.number.isRequired,
-  };
-
+const RightMenu: React.FC<Props> = ({ mode, cartItems }) => {
   return (
     <div data-testid="right-menu">
       <Menu defaultSelectedKeys={['search']} mode={mode}>
         <Menu.Item key="search">
           <Link to="/search">Search</Link>
         </Menu.Item>
-        <SubMenu
+        <Menu.SubMenu
           title={<span className="submenu-title-wrapper">Learn</span>}
           disabled
         >
@@ -28,12 +30,12 @@ function RightMenu({ mode, cartItems }) {
             <Menu.Item key="guide">Guide</Menu.Item>
             <Menu.Item key="api">API</Menu.Item>
           </Menu.ItemGroup>
-        </SubMenu>
+        </Menu.SubMenu>
         <Menu.Item key="resources" disabled>
           Resources
         </Menu.Item>
         <Menu.Item key="login">
-          <Button type="primary" style={{ fontStyle: 'bold' }} disabled>
+          <Button type="primary" disabled>
             Log In
           </Button>
         </Menu.Item>
@@ -48,6 +50,6 @@ function RightMenu({ mode, cartItems }) {
       </Menu>
     </div>
   );
-}
+};
 
 export default RightMenu;
