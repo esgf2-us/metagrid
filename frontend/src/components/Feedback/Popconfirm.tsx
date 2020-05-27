ffae12b634a509ebbc3f353961e21df6c2a6a582
tomvothecoder <tomvothecoder@gmail.com>
2020-05-27 11:08:27 -0700

    Convert Popconfirm component to .tsx

---
+++ frontend/src/components/Feedback/Popconfirm.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Popconfirm as PopconfirmD } from 'antd';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+
+type Props = {
+  title?: string;
+  icon?: React.ReactNode;
+  placement?:
+    | 'top'
+    | 'left'
+    | 'right'
+    | 'bottom'
+    | 'topLeft'
+    | 'topRight'
+    | 'bottomLeft'
+    | 'bottomRight'
+    | 'leftTop'
+    | 'leftBottom'
+    | 'rightTop'
+    | 'rightBottom'
+    | undefined;
+  onConfirm: () => void;
+  children: React.ReactElement;
+};
+
+const Popconfirm: React.FC<Props> = ({
+  title = 'Are you sure?',
+  icon = <ExclamationCircleOutlined />,
+  placement = 'top',
+  onConfirm,
+  children,
+}) => {
+  return (
+    <PopconfirmD
+      title={title}
+      icon={icon}
+      placement={placement}
+      onConfirm={onConfirm}
+    >
+      {children}
+    </PopconfirmD>
+  );
+};
+
+export default Popconfirm;
