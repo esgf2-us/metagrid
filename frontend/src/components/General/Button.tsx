37a0791a5066e83b44920d74438831a4fb9c538d
tomvothecoder <tomvothecoder@gmail.com>
2020-05-26 17:15:08 -0700

    Convert Button to TypeScript

---
+++ frontend/src/components/General/Button.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Button as ButtonD } from 'antd';
+
+interface Props {
+  type?: 'link' | 'ghost' | 'default' | 'primary' | 'dashed' | undefined;
+  className?: string | undefined;
+  href?: string | undefined;
+  target?: string | undefined;
+  htmlType?: 'button' | 'submit' | 'reset' | undefined;
+  icon?: React.ReactNode;
+  onClick?:
+    | ((event: React.MouseEvent<HTMLElement, MouseEvent>) => void)
+    | undefined;
+  disabled?: boolean | undefined;
+  children: React.ReactElement;
+}
+
+const Button: React.FC<Props> = ({
+  type = 'primary',
+  className,
+  href,
+  target,
+  icon,
+  htmlType,
+  onClick,
+  disabled = false,
+  children,
+}) => {
+  return (
+    <ButtonD
+      type={type}
+      className={className}
+      href={href}
+      target={target}
+      htmlType={htmlType}
+      icon={icon}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {children}
+    </ButtonD>
+  );
+};
+
+export default Button;
