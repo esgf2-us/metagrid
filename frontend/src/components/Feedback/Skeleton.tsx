52af1b58219db226c445c370433150ce09723a05
tomvothecoder <tomvothecoder@gmail.com>
2020-05-27 11:10:05 -0700

    Convert Skeleton component to .tsx

--- frontend/src/components/Feedback/Skeleton.tsx
+++ frontend/src/components/Feedback/Skeleton.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import { PropTypes } from 'prop-types';
 import { Skeleton as SkeletonD } from 'antd';
 
-const Skeleton = ({ active }) => {
+type Props = {
+  active?: boolean;
+};
+
+const Skeleton: React.FC<Props> = ({ active = false }) => {
   return (
     <div data-testid="skeleton">
       <SkeletonD active={active} />
     </div>
   );
-};
-
-Skeleton.propTypes = {
-  active: PropTypes.bool,
-};
-
-Skeleton.defaultProps = {
-  active: false,
 };
 
 export default Skeleton;
