cdbb72aee4344e7d6028a701926502ec8b307356
tomvothecoder <tomvothecoder@gmail.com>
2020-05-29 11:47:35 -0700

    Update Project type definition

--- frontend/src/custom_types/project.d.ts
+++ frontend/src/custom_types/project.d.ts
@@ -1,2 +1,2 @@
 // Type definition for a project, based on the MetaGrid Project API
-type Project = { [key: string]: string | string[] } | {};
+type Project = { [key: string]: string | string[] | number } | {};
