eec617d2e5adfaffa2562ba34018376184b7eaf8
tomvothecoder <tomvothecoder@gmail.com>
2020-05-29 16:01:03 -0700

    Convert utils to .ts

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,36 +1,35 @@
 /**
  * Checks if an object is empty.
- * @param {*} obj
  */
-export function isEmpty(obj) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const isEmpty = (obj: Record<any, any>): boolean => {
   return !obj || Object.keys(obj).length === 0;
-}
+};
 
 /**
  * Makes a string human-readable rather than underscore separated and lowercase.
- * @param {*} str
  */
-export function humanize(str) {
+export const humanize = (str: string): string => {
   const frags = str.split('_');
   for (let i = 0; i < frags.length; i += 1) {
     frags[i] = frags[i].charAt(0).toUpperCase() + frags[i].slice(1);
   }
   return frags.join(' ');
-}
+};
 
 /** Parses urls to remove characters following the specified character
- * @param {string} url
- * @param {string} char
  */
-export function parseUrl(url, char) {
+export const parseUrl = (url: string, char: string): string => {
   return url.split(char)[0];
-}
+};
 
 /**
  * Checks if the specified key is in the object
- * @param {object} obj
- * @param {string} key
  */
-export function hasKey(obj, key) {
-  return hasOwnProperty.call(obj, key);
-}
+export const hasKey = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  obj: Record<any, any>,
+  key: string | number
+): boolean => {
+  return Object.prototype.hasOwnProperty.call(obj, key);
+};
