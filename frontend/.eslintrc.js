b1cb5e5121d6f635c728f601456aaeaa36773307
tomvothecoder <tomvothecoder@gmail.com>
2020-06-01 16:16:55 -0700

    Update .eslintrc settings for post-TypeScript conversion

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -27,35 +27,17 @@ module.exports = {
       1,
       { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
     ],
+    'react/prop-types': 'off',
     'react/self-closing-comp': 'off',
-    // Disable these rules for all files since the project is mixed with JavaScript and TypeScript.
-    // Overrides are used to target TypeScript files only.
-    // TODO:: Remove these configurations once TypeScript conversion is fully completed
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/unbound-method': 'off',
+    '@typescript-eslint/explicit-function-return-type': ['error'],
+    // Makes no sense to allow type inferrence for expression parameters, but require typing the response (https://github.com/iamturns/create-exposed-app/blob/master/.eslintrc.js)
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      { allowExpressions: true, allowTypedFunctionExpressions: true },
+    ],
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      { functions: false, classes: true, variables: true, typedefs: true },
+    ],
   },
-  overrides: [
-    {
-      files: ['**/*.tsx'],
-      rules: {
-        'react/prop-types': 'off',
-      },
-    },
-    // https://github.com/typescript-eslint/typescript-eslint/issues/964
-    {
-      files: ['**/*.ts', '**/*.tsx'],
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': ['error'],
-        // Makes no sense to allow type inferrence for expression parameters, but require typing the response (https://github.com/iamturns/create-exposed-app/blob/master/.eslintrc.js)
-        '@typescript-eslint/explicit-function-return-type': [
-          'error',
-          { allowExpressions: true, allowTypedFunctionExpressions: true },
-        ],
-        '@typescript-eslint/no-use-before-define': [
-          'error',
-          { functions: false, classes: true, variables: true, typedefs: true },
-        ],
-      },
-    },
-  ],
 };
