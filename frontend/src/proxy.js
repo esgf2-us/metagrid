a1a79d6932d5bcbab29876ef8a2129518dc41eed
tomvothecoder <tomvothecoder@gmail.com>
2020-06-09 15:28:25 -0700

    Add environment variables configuration for frontend

--- frontend/src/proxy.js
+++ frontend/src/proxy.js
@@ -1,18 +1,21 @@
+require('dotenv').config();
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const corsProxy = require('cors-anywhere');
 
+// The protocol of the HOST
+const protocol = process.env.REACT_APP_PROXY_PROTOCOL;
 // Listen on a specific host via the HOST environment variable
-const host = process.env.HOST || '0.0.0.0';
+const host = process.env.REACT_APP_PROXY_HOST;
 // Listen on a specific port via the PORT environment variable
-const port = process.env.PORT || 8080;
+const port = process.env.REACT_APP_PROXY_PORT;
 
 corsProxy
   .createServer({
-    originWhitelist: ['http://localhost:3000'],
+    originWhitelist: process.env.REACT_APP_PROXY_ORIGIN_WHITELIST.split(' '),
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2'],
   })
   .listen(port, host, () => {
     // eslint-disable-next-line no-console
-    console.log(`Running CORS Anywhere on ${host}:${port}`);
+    console.log(`Running CORS Anywhere on ${protocol}${host}:${port}`);
   });
