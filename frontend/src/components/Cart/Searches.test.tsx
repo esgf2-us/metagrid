8a8189792f4801685e4c6d8b9f7fe90d649936d5
tomvothecoder <tomvothecoder@gmail.com>
2020-06-11 14:01:18 -0700

    Add dynamic result count fetching with SearchesCard component - Individual saved search states are now managed independently with SearchesCard - Update tests to reflect changes related to call to API

--- frontend/src/components/Cart/Searches.test.tsx
+++ frontend/src/components/Cart/Searches.test.tsx
@@ -1,23 +1,8 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
-import { MemoryRouter } from 'react-router-dom';
 import Searches, { Props } from './Searches';
-
-beforeEach(() => {
-  const mockHistoryPush = jest.fn();
-  jest.mock(
-    'react-router-dom',
-    () =>
-      ({
-        ...jest.requireActual('react-router-dom'),
-        useHistory: () => ({
-          push: mockHistoryPush,
-        }),
-      } as {})
-  );
-});
 
 afterEach(() => {
   jest.clearAllMocks();
@@ -44,41 +29,4 @@ it('renders component with empty savedSearches', () => {
 
   const emptyText = getByText('Your search library is empty');
   expect(emptyText).toBeTruthy();
-});
-
-it('renders component with savedSearches and handles button clicks', () => {
-  const { getByRole } = render(
-    <MemoryRouter>
-      <Searches {...defaultProps} />
-    </MemoryRouter>
-  );
-
-  // Check search button renders and click it
-  const searchBtn = getByRole('img', { name: 'search' });
-  expect(searchBtn).toBeTruthy();
-  fireEvent.click(searchBtn);
-
-  // Check delete button renders and click it
-  const deleteBtn = getByRole('img', { name: 'delete' });
-  expect(deleteBtn).toBeTruthy();
-  fireEvent.click(deleteBtn);
-});
-
-it('displays "N/A" when no constraints are applied', () => {
-  const { getByText } = render(
-    <Searches
-      {...defaultProps}
-      savedSearches={[
-        {
-          ...defaultProps.savedSearches[0],
-          activeFacets: {},
-          textInputs: [],
-        },
-      ]}
-    />
-  );
-
-  // Check search button renders
-  const queryString = getByText('N/A');
-  expect(queryString).toBeTruthy();
 });
