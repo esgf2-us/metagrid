68eb15e32bda88dd350891a8aa7ac19fd5ee7d8e
Tom Vo <tomvothecoder@gmail.com>
2021-01-25 10:49:27 -0800

    Move Django config folder from metagrid to root folder - Move Django settings files to config/settings - Update references to configs - Update docs - Closes #245

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -26,7 +26,7 @@ sys.path.append(os.path.join(app_path, ".."))
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings.production"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "metagrid.config.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
