7530c42b3fd9becba308ce2a4abc1a067b0b5718
Sasha Ames <amysash2006@gmail.com>
2022-05-12 13:59:04 -0700

    revamp for different request types

--- backend/metagrid/api_proxy/views.py
+++ backend/metagrid/api_proxy/views.py
@@ -16,29 +16,44 @@ def do_search(request):
 @require_http_methods(['GET', 'POST'])
 @csrf_exempt
 def do_citation(request):
-    return do_request(request, "")
+    url = request.POST["url"]
+    
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        return HttpResponse(resp.text)
+    else:
+        return HttpResponseBadRequest(resp.text)
+
+
 
 @require_http_methods(['GET', 'POST'])
 @csrf_exempt
 def do_status(request):
-    return do_request(request, "")
+    return do_request(request, "https://aims4.llnl.gov/prometheus/api/v1/query?query=probe_success%7Bjob%3D%22http_2xx%22%2C+target%3D~%22.%2Athredds.%2A%22%7D")
 
 @require_http_methods(['GET', 'POST'])
 @csrf_exempt
 def do_wget(request):
-    return do_request(request, "")
+    return do_request(None, "https://greyworm1.llnl.gov/wget")
 
 
 
 def do_request(request, urlbase):
-    if request.method == 'POST':
-        url_params = request.POST.copy()
-    elif request.method == 'GET':
-        url_params = request.GET.copy()
-    else:
-        return HttpResponseBadRequest('Request method must be POST or GET.')
+    resp = None
 
-    resp = requests.get(urlbase, params=url_params)
+    if (request):
+        if request.method == 'POST':
+            url_params = request.POST.copy()
+        elif request.method == 'GET':
+            url_params = request.GET.copy()
+        else:
+            return HttpResponseBadRequest('Request method must be POST or GET.')
+
+         
+        resp = requests.get(urlbase, params=url_params)
+    else:
+        resp = request.get(urlbase)
+        
     if resp.status_code == 200:
         return HttpResponse(resp.text)
     else:
