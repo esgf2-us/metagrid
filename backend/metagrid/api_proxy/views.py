f4fef3c6c3fc91c9a41e633ce8d87095e4306f88
Sasha Ames <sashakames@users.noreply.github.com>
2022-07-14 16:43:25 -0700

    hotfix to get wget script download to work (#449)
    * hotfix to get wget script download to work
    
    * fix removed return

--- backend/metagrid/api_proxy/views.py
+++ backend/metagrid/api_proxy/views.py
@@ -57,6 +57,7 @@ def do_wget(request):
     return do_request(request, "https://esgf-node.llnl.gov/esg-search/wget")
 
 
+
 def do_request(request, urlbase):
     resp = None
 
@@ -76,6 +77,6 @@ def do_request(request, urlbase):
 
     httpresp = HttpResponse(resp.text)
     httpresp.status_code = resp.status_code
-    #    httpresp.headers = resp.headers
-    #    httpresp.encoding = resp.encoding
+    httpresp.headers = resp.headers
+    httpresp.encoding = resp.encoding
     return httpresp
