326531a0366e6e73fbcf66b4fe19fe0cc8f1e761
Sasha Ames <amysash2006@gmail.com>
2022-10-12 16:10:43 -0700

    add missing import

--- backend/metagrid/api_proxy/views.py
+++ backend/metagrid/api_proxy/views.py
@@ -2,6 +2,7 @@ import json
 from urllib.parse import urlparse
 
 import requests
+from django.conf import settings
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
