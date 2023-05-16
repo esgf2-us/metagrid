cf20f53387e1d7e4e89ec6443d9577bacca2b8fc
Carlos Downie <42552189+downiec@users.noreply.github.com>
2023-05-16 11:14:02 -0700

    Updated the general facet order for input 4 mips

--- backend/metagrid/initial_projects_data.py
+++ backend/metagrid/initial_projects_data.py
@@ -148,9 +148,9 @@ projects = [
         "description": "input4MIPS (input datasets for Model Intercomparison Projects) is an activity to make available via ESGF the boundary condition and forcing datasets needed for CMIP6. Various datasets are needed for the pre-industrial control (piControl), AMIP, and historical simulations, and additional datasets are needed for many of the CMIP6-endorsed model intercomparison projects (MIPs) experiments. Earlier versions of many of these datasets were used in the 5th Coupled Model Intercomparison Project (CMIP5).",
         "facets_by_group": {
             GROUPS[0]: [
+                "mip_era",
                 "target_mip_list",
                 "dataset_status",
-                "mip_era",
                 "data_node",
             ],
             GROUPS[1]: [
