97c8273372276f4192b1c75c80cd8a4cba1e8cda
downiec <42552189+downiec@users.noreply.github.com>
2024-01-23 16:18:38 -0800

    Merged in latest v1.1.0 branch and fixed some linting errors in backend

--- backend/metagrid/api_globus/views.py
+++ backend/metagrid/api_globus/views.py
@@ -6,7 +6,11 @@ import urllib.request
 from datetime import datetime, timedelta
 
 from django.conf import settings
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseServerError
+from django.http import (
+    HttpResponse,
+    HttpResponseBadRequest,
+    HttpResponseServerError,
+)
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from globus_sdk import AccessTokenAuthorizer, TransferClient, TransferData
@@ -14,16 +18,12 @@ from globus_sdk import AccessTokenAuthorizer, TransferClient, TransferData
 from metagrid.api_proxy.views import do_request
 
 ENDPOINT_MAP = {
-    "415a6320-e49c-11e5-9798-22000b9da45e" : "1889ea03-25ad-4f9f-8110-1ce8833a9d7e"
+    "415a6320-e49c-11e5-9798-22000b9da45e": "1889ea03-25ad-4f9f-8110-1ce8833a9d7e"
 }
 
-DATANODE_MAP = {
-    "aims3.llnl.gov" : "1889ea03-25ad-4f9f-8110-1ce8833a9d7e"
-}
+DATANODE_MAP = {"aims3.llnl.gov": "1889ea03-25ad-4f9f-8110-1ce8833a9d7e"}
 
-TEST_SHARDS_MAP = {
-    "esgf-fedtest.llnl.gov" : "esgf-node.llnl.gov"
-}
+TEST_SHARDS_MAP = {"esgf-fedtest.llnl.gov": "esgf-node.llnl.gov"}
 
 
 # reserved query keywords
@@ -137,7 +137,9 @@ def get_files(url_params):  # pragma: no cover
     use_distrib = True
 
     try:
-        hostname = urllib.parse.urlparse(query_url).hostname  # TODO need to populate the sharts based on the Solr URL
+        hostname = urllib.parse.urlparse(
+            query_url
+        ).hostname  # TODO need to populate the sharts based on the Solr URL
     except RuntimeError as e:
         return HttpResponseServerError(f"Malformed URL in search results {e}")
     if hostname in TEST_SHARDS_MAP:
@@ -347,7 +349,7 @@ def do_globus_transfer(request):  # pragma: no cover
         parts = file.split("/")
         if data_node in DATANODE_MAP:
             endpoint_id = DATANODE_MAP[data_node]
-            print("Data node mapping.....") 
+            print("Data node mapping.....")
         else:
             endpoint_id = parts[0]
             if endpoint_id in ENDPOINT_MAP:
@@ -365,7 +367,6 @@ def do_globus_transfer(request):  # pragma: no cover
     print()
 
     for source_endpoint, source_files in list(download_map.items()):
-
         # submit transfer request
         task_id = submit_transfer(
             transfer_client,
