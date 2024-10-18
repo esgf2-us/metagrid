186d1e5645dddc823d59dbfe0deec215121d17eb
downiec <42552189+downiec@users.noreply.github.com>
2024-10-17 17:11:03 -0700

    add no coverage for status url. Update local and production docker images so they match, using python 3.12

--- backend/metagrid/api_proxy/views.py
+++ backend/metagrid/api_proxy/views.py
@@ -142,7 +142,7 @@ def do_status(request):
         settings,
         "STATUS_URL",
         "",
-    )
+    )  # pragma: no cover
     resp = requests.get(status_url)  # pragma: no cover
     if resp.status_code == 200:  # pragma: no cover
         return HttpResponse(resp.text)
