e9cc9ae7c52763ca09826573ed67da43a6f9377b
downiec <42552189+downiec@users.noreply.github.com>
2025-01-16 13:35:14 -0800

    Fixed some linting errors related to unused imports

--- frontend/src/components/Search/index.test.tsx
+++ frontend/src/components/Search/index.test.tsx
@@ -1,4 +1,4 @@
-import { act, within, screen } from '@testing-library/react';
+import { within, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import {
