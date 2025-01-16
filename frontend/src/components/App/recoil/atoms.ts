c0d8a18fd7d04e324d3b8ca73006d6929d86a4e8
Zach Price <pricezt@ornl.gov>
2025-01-15 20:34:22 -0500

    Persist theme choice

--- frontend/src/components/App/recoil/atoms.ts
+++ frontend/src/components/App/recoil/atoms.ts
@@ -1,8 +1,24 @@
-import { atom } from 'recoil';
+import { atom, AtomEffect } from 'recoil';
+
+export const localStorageEffect = (key: string): AtomEffect<boolean> => ({ setSelf, onSet }) => {
+  const savedValue = localStorage.getItem(key);
+  if (savedValue != null) {
+    setSelf(savedValue === 'true');
+  }
+
+  onSet((newValue, _, isReset) => {
+    if (isReset) {
+      localStorage.removeItem(key);
+    } else {
+      localStorage.setItem(key, newValue.toString());
+    }
+  });
+};
 
 export const isDarkModeAtom = atom<boolean>({
   key: 'isDarkMode',
   default: false,
+  effects: [localStorageEffect('isDarkMode')],
 });
 
 export default isDarkModeAtom;
