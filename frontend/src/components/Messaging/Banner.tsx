341502ed074517b614fc1c70b0059905730f5be3
downiec <42552189+downiec@users.noreply.github.com>
2025-05-05 17:57:55 -0700

    Updated package.json to remove unused Recoil dependency. Updated some minor packages. Updated the Banner component to stay hidden after uses clicks the close button, even after a refresh, by using localstorage variable. Added tests to covere the banner component feature.

--- frontend/src/components/Messaging/Banner.tsx
+++ frontend/src/components/Messaging/Banner.tsx
@@ -4,14 +4,15 @@ import { CloseSquareTwoTone } from '@ant-design/icons';
 import { useAtomValue } from 'jotai';
 import { isDarkModeAtom } from '../../common/atoms';
 import { darkModeRed, lightModeRed } from '../NodeStatus/StatusToolTip';
+import { showBanner, saveBannerText } from '../../common/utils';
 
 const Banner: React.FC = () => {
   const isDarkMode = useAtomValue<boolean>(isDarkModeAtom);
 
-  const [isVisible, setIsVisible] = useState(true);
+  const [isVisible, setIsVisible] = useState(showBanner());
   const bannerText = window.METAGRID.BANNER_TEXT;
 
-  if (!isVisible || bannerText === null || bannerText === '') {
+  if (!isVisible) {
     return <></>;
   }
 
@@ -21,7 +22,11 @@ const Banner: React.FC = () => {
         <CloseSquareTwoTone
           twoToneColor={isDarkMode ? darkModeRed : lightModeRed}
           style={{ fontSize: '20px' }}
-          onClick={() => setIsVisible(false)}
+          onClick={() => {
+            setIsVisible(false);
+            // Save the banner text so it doesn't show again
+            saveBannerText();
+          }}
         />{' '}
         {bannerText}
       </h2>
